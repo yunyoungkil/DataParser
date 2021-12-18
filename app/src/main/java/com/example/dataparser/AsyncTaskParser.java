250683e828b9864021b4ebc9cf0320a782764338
yunyo <yunyoungkil76@gmail.com>
2021-12-18 16:59:54 +0900

    회사에서

--- app/src/main/java/com/example/dataparser/AsyncTaskParser.java
+++ app/src/main/java/com/example/dataparser/AsyncTaskParser.java
@@ -3,6 +3,9 @@ package com.example.dataparser;
 import android.os.AsyncTask;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -12,10 +15,25 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+
 public class AsyncTaskParser extends AsyncTask<String, Void, JSONArray> {
+    /**
+     필드명	설명	타입
+     market	업비트에서 제공중인 시장 정보	String
+     korean_name	거래 대상 암호화폐 한글명	String
+     english_name	거래 대상 암호화폐 영문명	String
+     market_warning	유의 종목 여부
+     NONE (해당 사항 없음), CAUTION(투자유의)	String
+
+     isDetails boolean =false,true
+     유의종목 필드과 같은 상세 정보 노출 여부(선택 파라미터)
+     */
+
     JSONArray d_jsonArray;
 
     // The TextView where we will show results
@@ -23,25 +41,14 @@ public class AsyncTaskParser extends AsyncTask<String, Void, JSONArray> {
     private WeakReference<ListView> mListView;
 
     // Constructor that provides a reference to the TextView from the MainActivity
-    AsyncTaskParser(TextView tv) {
+    AsyncTaskParser(TextView tv, ListView tv1) {
        mTextView = new WeakReference<>(tv);
+       mListView = new WeakReference<>(tv1);
     }
 
     @Override
     protected JSONArray doInBackground(String... string)  {
 
-
-        /**
-        필드명	설명	타입
-        market	업비트에서 제공중인 시장 정보	String
-        korean_name	거래 대상 암호화폐 한글명	String
-        english_name	거래 대상 암호화폐 영문명	String
-        market_warning	유의 종목 여부
-        NONE (해당 사항 없음), CAUTION(투자유의)	String
-
-         isDetails boolean =false,true
-         유의종목 필드과 같은 상세 정보 노출 여부(선택 파라미터)
-         */
 
         Response response = null;
         OkHttpClient client = new OkHttpClient();
@@ -53,11 +60,10 @@ public class AsyncTaskParser extends AsyncTask<String, Void, JSONArray> {
                 .build();
 
         try {
-
             response = client.newCall(request).execute();
             String s = response.body().string();
            d_jsonArray = new JSONArray(s);
-
+            /*
             for (int i =0; i < d_jsonArray.length(); i++){
                 JSONObject jdata = d_jsonArray.getJSONObject(i);
                 String market = jdata.getString("market");
@@ -65,7 +71,9 @@ public class AsyncTaskParser extends AsyncTask<String, Void, JSONArray> {
                 String english_name = jdata.getString("english_name");
 
             }
-           //  Log.d("마켓" ,"이름: "+ korean_name);
+
+           Log.d("마켓" ,"이름: "+ korean_name);
+            */
         } catch (IOException | JSONException e) {
             e.printStackTrace();
         }
--- app/src/main/java/com/example/dataparser/MainActivity.java
+++ app/src/main/java/com/example/dataparser/MainActivity.java
@@ -2,11 +2,22 @@ package com.example.dataparser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,14 +28,36 @@ public class MainActivity extends AppCompatActivity {
 
     // The TextView where we will show results
     private TextView mTextView;
+    private ListView mListView;
+
+    private ArrayList<Object> arrayList;
+    /*
+    import java.util.ArrayList;
+    ArrayList<Integer> integers1 = new ArrayList<Integer>(); // 타입 지정
+    ArrayList<Integer> integers2 = new ArrayList<>(); // 타입 생략 가능
+    ArrayList<Integer> integers3 = new ArrayList<>(10); // 초기 용량(Capacity) 설정
+    ArrayList<Integer> integers4 = new ArrayList<>(integers1); // 다른 Collection값으로 초기화
+    ArrayList<Integer> integers5 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5)); // Arrays.asList()
+
+    .add("Black");  데이터 추가
+    .set(0, "Blue"); 인덱스 0에 Black를 Blue로 변경
+    .remove("White"); White 삭제
+    .size() 크기 확인
+    .clear(); 내용 전체 삭제
+     String removedColor = colors.remove(0); 데이터 인덱스 0번을 리턴
+     boolean contains = colors.contains("Black"); 데이터 존재 여부 확인 =>있으면 true,없으면 false
+     int index = colors.indexOf("Blue"); 값이 존재하는 경우 인덱스를 리턴,값이 존재하지 않을 경우 -1을 리턴
+    */
+    private ArrayAdapter<Object> arrayAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView = findViewById(R.id.textView1);
 
+        mTextView = findViewById(R.id.textView1);
+        mListView = findViewById(R.id.listview_list);
 
         // Restore TextView if there is a savedInstanceState bundle.
         if (savedInstanceState != null) {
@@ -32,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-
     /**
      * Handles the onClick for the "Start Task" button. Launches the AsyncTask
      * which performs work off of the UI thread.
@@ -41,14 +73,28 @@ public class MainActivity extends AppCompatActivity {
      */
     public void startTask(View view) {
 
-
-
         // Put a message in the text view.
         mTextView.setText(R.string.napping);
 
         // Start the AsyncTask.
         // The AsyncTask has a callback that will update the text view.
-        new AsyncTaskParser(mTextView).execute();
+
+        AsyncTask<String, Void, JSONArray> mAsyncTaskParser_JsonData = new AsyncTaskParser(mTextView,mListView).execute();
+        arrayList = new ArrayList<>();
+        try {
+            JSONObject jdata = null;
+            for (int i =0; i < mAsyncTaskParser_JsonData.get().length(); i++){
+
+                    jdata = mAsyncTaskParser_JsonData.get().getJSONObject(i);
+                    arrayList.add(jdata.getString("korean_name"));
+            }
+            arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,arrayList);
+            mListView.setAdapter(arrayAdapter);
+            Log.d("====================",""+ arrayList.toString());
+
+        } catch (ExecutionException | InterruptedException | JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -63,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
         // Save the state of the TextView
         outState.putString(TEXT_STATE, mTextView.getText().toString());
 
-         Log.d("마켓: " +outState,"이름: ");
+
+
     }
 }
